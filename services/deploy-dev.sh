4d2105185e5a367178f80f4d7d03b2d16caea8f9
Maqbool ur Rahim Khan <maqboolurrahim@gmail.com>
2020-07-22 11:37:24 +0200

    feat(ida-19): project is now uses microservice architecture and also updated README

--- services/deploy-dev.sh
+++ services/deploy-dev.sh
@@ -2,5 +2,23 @@
 
 cd "${BASH_SOURCE%/*}" || exit
 
-docker build -t ida .. -f backend-server/Dockerfile.dev
-docker run -p 8090:8090 ida
+version=$(cat VERSION)
+registry=${REGISTRY:-localhost:5000}
+
+function build_container() {
+    tag=$registry/ida/$1:$version
+    echo "Building $1: $tag..."
+    if docker build ${@:2} -t $tag; then
+        echo "Successfully built $tag."
+        if docker push $tag; then
+            echo "Successfully pushed $tag."
+        fi
+    fi
+}
+
+build_container backend-server .. -f backend-server/Dockerfile.dev
+
+export REGISTRY=$registry
+export VERSION=$version
+
+docker stack deploy --compose-file docker-compose-dev.yml ida-stack-dev
