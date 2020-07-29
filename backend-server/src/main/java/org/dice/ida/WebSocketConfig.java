5519d91a0eba7b75fdd64c4686f431cfb77797a0
Maqbool ur Rahim Khan <maqboolurrahim@gmail.com>
2020-07-29 13:12:30 +0200

    feat(ida-19): code compliances and codacy fix

--- backend-server/src/main/java/org/dice/ida/WebSocketConfig.java
+++ backend-server/src/main/java/org/dice/ida/WebSocketConfig.java
@@ -12,9 +12,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		String ui_url = "http://localhost:3000"; // it should come from application properties
 		// This end point will be used if sockets are not available
-		registry.addEndpoint("/ida-fb-ws").setAllowedOrigins(ui_url).withSockJS();
+		registry.addEndpoint("/ida-fb-ws").setAllowedOrigins("*").withSockJS();
 	}
 
 	@Override
