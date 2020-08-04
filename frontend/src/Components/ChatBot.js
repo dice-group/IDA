005e4a6fbe36e0ec45be73fd6657dd1299fc8eb4
spoorthimadhavan <spoorthimadhavan@users.noreply.github.com>
2020-08-04 12:40:28 +0200

    added datatable component

--- frontend/src/Components/ChatBot.js
+++ frontend/src/Components/ChatBot.js
@@ -1,20 +1,26 @@
 import React, {Component} from 'react'
 import axios from 'axios';
 import {Launcher} from 'react-chat-window'
- 
+import './chatbot.css';
+import CustomizedTables from './Datatable'
+import Treeview from './Treeview'
+
 export default class Demo extends Component {
  
   constructor() {
     super();
     this.state = {
-      messageList: []
-    };
+      messageList: [],
+      changeCSS:{}
+    }
   }
  
   _onMessageWasSent(message) {
     this.setState({
       messageList: [...this.state.messageList, message]
     })
+    // let changeCSS = {top:'50%' , transform: 'translate(-50%, -50%)'}
+
     var outerscope = this;
     if (message.data.text.length > 0) {
         let obj = {
@@ -23,12 +29,18 @@ export default class Demo extends Component {
             "timestamp" : "",
             "senderName" : "spoorthi"
         };
-        console.log(obj);
+        console.log(obj,message.data.text);
+        if(message.data.text === "upload dataset") {
+        
+        }
         const res = axios.post('http://localhost:8090/chatmessage', obj, {}
         ).then(response => {
             console.log('success');
             console.log(response);
+            console.log("this is testing");
+            //changeCSS = {top:'50%' , right: 0, transform: 'translate(-10%, -60%) !important'};
             outerscope._sendMessage(response.data.message)
+            
       }, function (err) {
             console.log('error');
             console.log(err.status);
@@ -47,16 +59,27 @@ export default class Demo extends Component {
   }
  
   render() {
-    return (<div>
+    let changeCSS = {top:'50%' , transform: 'translate(-50%, -50%)'}
+
+    return (
+    <div style={{changeCSS}} >
       <Launcher
         agentProfile={{
           teamName: 'IDA-ChatBot',
-          imageUrl: ''
+          imageUrl: '',
+          className:{changeCSS}
+          
         }}
         onMessageWasSent={this._onMessageWasSent.bind(this)}
         messageList={this.state.messageList}
-        showEmoji
       />
-    </div>)
+    
+    <div>
+        {/* <Treeview/>
+        <CustomizedTables/>  */}
+    </div>
+    </div>
+    )
+   
   }
 }
