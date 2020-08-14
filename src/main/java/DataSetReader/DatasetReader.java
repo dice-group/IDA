8ea1c073b466c44d404eee7c1f403f25fe5376c6
sourabhpoddar404 <sourabh.poddar404@gmail.com>
2020-08-14 14:56:43 +0200

    meta file writer changes

--- src/main/java/DataSetReader/DatasetReader.java
+++ src/main/java/DataSetReader/DatasetReader.java
@@ -3,10 +3,12 @@ package DataSetReader;
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
-
+import java.util.Enumeration;
 import model.AttributeSummary;
 import model.DatasetSummary;
 import model.VisualizationSuggestion;
+import weka.core.Attribute;
+import weka.core.AttributeStats;
 import weka.core.Instances;
 import weka.core.converters.CSVLoader;
 
@@ -14,17 +16,49 @@ public class DatasetReader {
 
     private String fileName;
     Instances data;
-
-    public DatasetSummary createMetainfo() throws IOException {
+    DatasetSummary datasetSummary;
+    public void createMetainfo() throws IOException {
         int index = fileName.indexOf(".");
-        Calendar c = Calendar.getInstance();
         fileName = fileName.substring(0, index);
-        BufferedWriter writer = new BufferedWriter(new FileWriter("./src/main/metainfo/" + fileName + c.get(Calendar.MONTH) + c.get(Calendar.YEAR)));
-        writer.write(data.toSummaryString());
+        List<Attribute> attributes = new ArrayList<Attribute>(); 
+        BufferedWriter writer = new BufferedWriter(new FileWriter("./src/main/metainfo/" + fileName));
+        for(int i=0;i<data.numAttributes();i++)
+           	attributes.add(data.attribute(i));
+        StringBuilder str = new StringBuilder();
+        str.append("Number of records\t"+data.numInstances()+"\n");
+        str.append("Number of Attributes\t"+ data.numAttributes()+"\n");
+        str.append("Name\t\tType\tNom\tInt\tReal\tMissing\tUnique\tDist"+"\n");
+        int k=0;
+        for (Attribute s:attributes)
+        	{
+        		AttributeStats as =  data.attributeStats(k);
+        		String stat =  as.toString().split("\\n")[1];
+        		String fields[] = stat.split("\\s+");
+        		if(fields[6].length()<=1)
+        		{
+        			if(fields[9].length()<=1)
+        				str.append(s.name()+"\t\t"+fields[1]+"\t"+fields[2]+"\t"+fields[3]+"\t"+fields[4]+"\t"+fields[5]+"/"+fields[7]+"\t"+fields[8]+"/"+fields[10]+"\t"+fields[11]+"\n");
+        			if(fields[9].length()>1)
+        				str.append(s.name()+"\t\t"+fields[1]+"\t"+fields[2]+"\t"+fields[3]+"\t"+fields[4]+"\t"+fields[5]+"/"+fields[7]+"\t"+fields[8]+fields[9]+"\t"+fields[10]+"\n");
+        		}
+        		if(fields[6].length()>1)
+        		{
+        			if (fields[8].length()<=1)
+        				str.append(s.name()+"\t\t"+fields[1]+"\t"+fields[2]+"\t"+fields[3]+"\t"+fields[4]+"\t"+fields[5]+fields[6]+"\t"+fields[7]+"/"+fields[9]+"\t"+fields[10]+"\n");
+        			if (fields[8].length()>1)
+        				str.append(s.name()+"\t\t"+fields[1]+"\t"+fields[2]+"\t"+fields[3]+"\t"+fields[4]+"\t"+fields[5]+fields[6]+"\t"+fields[7]+fields[8]+"\t"+fields[9]+"\n");
+        		}
+        		k++;
+        	}
+        writer.write(str.toString());
         writer.close();
+        createDataSetSummary(fileName);
+    }
+    public void createDataSetSummary(String metaFileName)
+    {
         StringReader stringReader = new StringReader(data.toSummaryString());
         String[] summaryLines = data.toSummaryString().split("\n");
-        DatasetSummary datasetSummary = new DatasetSummary();
+        datasetSummary = new DatasetSummary();
         datasetSummary.setName(summaryLines[0].split(":")[1].trim());
         datasetSummary.setNumberOfInstances(Long.parseLong(summaryLines[1].split(":")[1].trim()));
         String[] attributeValues_1;
@@ -51,19 +85,23 @@ public class DatasetReader {
         }
         datasetSummary.setAttributeSummaryList(attributeSummaryList);
         suggestViz(datasetSummary);
-        return datasetSummary;
+       
     }
 
     public void FileReader(String fileName) throws IOException {
         this.fileName = fileName;
         CSVLoader loader = new CSVLoader();
         //Loads the File
-
+        
         loader.setSource(new File("./src/main/resources/" + fileName));
         //Create an instance of the dataset 
         data = loader.getDataSet();
         //Set class index of dataset
-        data.setClassIndex(data.numAttributes() - 1);
+        
+        boolean exists = new File("./src/main/metainfo/" + fileName.split("\\.")[0]).exists();
+        if(!exists)
+        	createMetainfo(); 
+       
     }
 
     public VisualizationSuggestion suggestViz(DatasetSummary datasetSummary) {
@@ -80,7 +118,7 @@ public class DatasetReader {
                         .filter(x -> "Num".equals(x.getType()) && x.getUniqueValuesProbability() > 90.00)
                         .map(AttributeSummary::getName).collect(Collectors.toList()));
             } else if ((datasetSummary.getNumberOfInstances() / attrributeSummary.getDiscreteValuesCount()) > 30) {
-                paramMap.put(attrributeSummary, new ArrayList<>() {{
+                paramMap.put(attrributeSummary, new ArrayList<String>() {{
                     add("count of " + attrributeSummary.getName());
                 }});
             }
