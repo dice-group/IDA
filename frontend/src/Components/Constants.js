b95d95f4173c6a30ee9a72c56bb09d6f4c6ac142
spoorthimadhavan <spoorthimadhavan@users.noreply.github.com>
2020-08-21 21:14:07 +0200

    ida-32-dataset-load-ui added message constant

--- frontend/src/Components/Constants.js
+++ frontend/src/Components/Constants.js
@@ -1,8 +1,23 @@
 import React from "react";
 export default class Constants{
     uiActions ={
+        /**
+         *  UI action codes
+         */
+	    // Load message normally
         UAC_NrmlMsg = 1001,
-        
-
+        // Load dataset
+        UIA_LOADDS = 1004,
+        // Open Upload dataset modal window
+        UAC_UPLDDTMSG = 1002,
+        /**
+         *  Pre-defined action codes
+         */
+        // Know more button
+        PDAC_KNWMR = 2001,
+        // Upload dataset button
+        PDAC_UPLDDT = 2002,
+        // View existing datasets button
+        PDAC_VWDTS = 2003,
     }
 }
