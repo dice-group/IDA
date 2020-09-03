7eb07a53327f716ca94a88b096c222bb4f285479
sourabhpoddar404 <sourabh.poddar404@gmail.com>
2020-09-03 17:07:15 +0200

    Bar Graph

--- backend-server/src/main/java/org/dice/ida/action/def/BarGraphAction.java
+++ backend-server/src/main/java/org/dice/ida/action/def/BarGraphAction.java
@@ -4,9 +4,15 @@ import java.io.File;
 import java.util.Map;
 import org.dice.ida.constant.IDAConst;
 import org.dice.ida.model.ChatMessageResponse;
+import org.dice.ida.model.bargraph.BarGraphData;
 import org.dice.ida.visualizer.BarGraphVisualizer;
 import weka.core.Instances;
 import weka.core.converters.CSVLoader;
+/**
+ * Class to perform action on bar-graph intent detection.
+ *
+ * @author Sourabh
+ */
 
 public class BarGraphAction implements Action {
 
@@ -38,6 +44,7 @@ public class BarGraphAction implements Action {
 				{
 					String xAxis = paramMap.get(IDAConst.PARAM_XAXIS_NAME).toString();
 					String yAxis = paramMap.get(IDAConst.PARAM_YAXIS_NAME).toString();
+					BarGraphData barGraph;
 					
 					if(xAxis != null && !xAxis.isEmpty() && yAxis != null && !yAxis.isEmpty())
 					{
@@ -47,14 +54,28 @@ public class BarGraphAction implements Action {
 					        loader.setSource(new File("./src/main/resources/datasets/" + datasetName +"/"+tableName));
 					        Instances data = loader.getDataSet();
 					       for (int i=0;i<data.numAttributes();i++) {
-							  if(data.attribute(i).name().trim().equalsIgnoreCase(xAxis)) xaxist=true;
-							  if(data.attribute(i).name().trim().equalsIgnoreCase(yAxis)) yaxist=true;
+							  if(data.attribute(i).name().substring(1).trim().equalsIgnoreCase(xAxis.trim()))
+								  {
+								  	xAxis = data.attribute(i).name();
+								  	xaxist=true;
+								  }
+							  if(data.attribute(i).name().trim().equalsIgnoreCase(yAxis.trim())) 
+								  {
+								  	yAxis = data.attribute(i).name();
+								  	yaxist=true;
+								  }
 					       }
 					    	if(xaxist && yaxist)
-					    		new BarGraphVisualizer().createBarGraph(xAxis, yAxis, datasetName, tableName);
+					    	{
+					    		barGraph = new BarGraphVisualizer(xAxis, yAxis, datasetName, tableName, data).createBarGraph();
+					    		payload.put("BarGraphData", barGraph);
+					    		chatMessageResponse.setPayload(payload); 
+					    		chatMessageResponse.setUiAction(IDAConst.UIA_BARGRAPH);
+					    	}
 					}
 					else
 					SimpleTextAction.setSimpleTextResponse(paramMap, chatMessageResponse);
+					
 				}
 			}
 			
--- backend-server/src/main/java/org/dice/ida/visualizer/BarGraphVisualizer.java
+++ backend-server/src/main/java/org/dice/ida/visualizer/BarGraphVisualizer.java
@@ -2,26 +2,79 @@ package org.dice.ida.visualizer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.dice.ida.model.AttributeSummary;
+import org.dice.ida.model.DataSummary;
 import org.dice.ida.model.bargraph.BarGraphData;
 import org.dice.ida.model.bargraph.BarGraphItem;
+import org.dice.ida.util.MetaFileReader;
+import weka.core.Attribute;
+import weka.core.Instances;
+/**
+ * Class to provide required attributes for bar graph visualization and apply data filters.
+ *
+ * @author Sourabh
+ */
 
 public class BarGraphVisualizer {
-	
-	public BarGraphData createBarGraph(String xAxis, String yAxis, String dsName,
-			String tableName)
+	String xAxisLabel;
+	String yAxisLabel;
+	String label = "Bar Graph";
+	String dataSetName;
+	String tableName;
+	Attribute xaxis ;
+	Attribute yxaxis;
+	List<BarGraphItem> items;
+	Instances data;
+	DataSummary DS;
+	public BarGraphVisualizer(String xAxis, String yAxis, String dsName,
+			String tableName, Instances data)
 	{
-		String xAxisLabel=null;
-		String yAxisLabel=null;
-		String label = null;
-		List<BarGraphItem> items = new ArrayList<BarGraphItem>();
+		this.xAxisLabel = xAxis;
+		this.yAxisLabel = yAxis;
+		this.dataSetName = dsName;
+		this.tableName = tableName;
+		this.data = data;
+		try {
+			DS = new MetaFileReader().createDataSummary(dataSetName, tableName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public BarGraphData createBarGraph()
+	{
+		
+		items = new ArrayList<BarGraphItem>();
 		loadBarGraphItem();  
 		
-		return new BarGraphData(label, items , xAxisLabel, yAxisLabel,dsName,
+		return new BarGraphData(label, items , xAxisLabel, yAxisLabel,dataSetName,
 				tableName);
 	}
 	private void loadBarGraphItem()
 	{
 		//check for filter and load bar graph items 
+		
+        //Loads the File
+			xaxis = data.attribute(xAxisLabel);
+			yxaxis = data.attribute(xAxisLabel);
+			if (xaxis.isNominal())
+			{
+				loadNominal(); 
+			}
+			if (xaxis.isNumeric())
+			{
+				
+			}
+			if (xaxis.isDate())
+			{
+				
+			}
+	}
+	public void loadNominal()
+	{
+		List<AttributeSummary> attributeSummary =DS.getAttributeSummaryList();
+		
 	}
 }
