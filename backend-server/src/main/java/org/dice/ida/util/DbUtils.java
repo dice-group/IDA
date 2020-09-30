a5a2634a9f7700079ae49dc16dd40d631cf2fe7e
Maqbool ur Rahim Khan <maqboolurrahim@gmail.com>
2020-09-30 19:18:55 +0500

    fix(ida-86): PR compliance, ida-unknown is now moved to constants

--- backend-server/src/main/java/org/dice/ida/util/DbUtils.java
+++ backend-server/src/main/java/org/dice/ida/util/DbUtils.java
@@ -1,7 +1,9 @@
 package org.dice.ida.util;
 
+import org.dice.ida.constant.IDAConst;
+
 public class DbUtils {
 	public static String manageNullValues(String value) {
-		return value.equals("?") ? "<ida-unk>" : value;
+		return (value == null || ValidatorUtil.isStringEmpty(value) || value.equals("?")) ? IDAConst.NULL_VALUE_IDENTIFIER : value;
 	}
 }
