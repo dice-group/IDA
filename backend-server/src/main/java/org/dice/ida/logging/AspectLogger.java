be6bedea1243b3fd196d10367867893f18f7b20f
Maqbool ur Rahim Khan <maqboolurrahim@gmail.com>
2020-10-30 14:58:22 +0500

    feat(ida-98): removed websocket controller from pointcut to solve mvn test failing issue

--- backend-server/src/main/java/org/dice/ida/logging/AspectLogger.java
+++ backend-server/src/main/java/org/dice/ida/logging/AspectLogger.java
@@ -29,7 +29,7 @@ public class AspectLogger {
 	 * Creates the point cut for the MessageRestController
 	 *
 	 */
-	@Pointcut("execution(* org.dice.ida.controller.*.*(..))")
+	@Pointcut("execution(* org.dice.ida.controller.MessageController.*(..))")
 	public void controller() {
 		//PointCut for MessageRestController
 		log.info("[EXECUTION] controller execution started");
