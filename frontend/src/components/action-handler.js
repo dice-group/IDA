24b41d5a28a14facd648f4fd0217804e8bb4c819
nandeesh <nandeesh@campus.uni-paderborn.de>
2020-12-02 15:28:43 +0100

    fixing codacy issue

--- frontend/src/components/action-handler.js
+++ frontend/src/components/action-handler.js
@@ -140,7 +140,7 @@ export default function IDAChatbotActionHandler(props, actionCode, payload) {
         }
         case IDA_CONSTANTS.UI_ACTION_CODES.UAC_CLUSTERING: {
             const clusteredData = payload.clusteredData;
-            clusteredData.sort((a, b) => parseInt(a.Cluster) > parseInt(b.Cluster) ? 1 : parseInt(a.Cluster) < parseInt(b.Cluster) ? -1 : 0);
+            clusteredData.sort((a, b) => parseInt(a.Cluster, 10) > parseInt(b.Cluster, 10) ? 1 : parseInt(a.Cluster, 10) < parseInt(b.Cluster, 10) ? -1 : 0);
             addAnalysisEntry(props, clusteredData, "Clustering", "clustering", payload.activeDS);
             break;
         }
