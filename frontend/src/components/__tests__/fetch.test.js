83e50755da531d4df9ac959473b6c87bd8224f94
Spoorthi S Madhavan <36541492+spoorthimadhavan@users.noreply.github.com>
2021-01-04 17:00:44 +0100

    Update fetch.test.js

--- frontend/src/components/__tests__/fetch.test.js
+++ frontend/src/components/__tests__/fetch.test.js
@@ -4,9 +4,10 @@ import { render, fireEvent } from "@testing-library/react";
 import Home from "../home";
 
 it("should accept the input", () => {
-    const mockMsg = jest.fn();
+//     const mockMsg = jest.fn();
+	const mockMsg ="Hello Ida";
     const { debug, getByLabelText } = render(<Home ChatApp={mockMsg} />);
     const Input = getByLabelText(/chat-input/i);
-    fireEvent.change(Input, { target: { placeholder: 'a new chat-input' } });
+    fireEvent.change(Input, { target: { placeholder: "a new chat-input" } });
     debug();
 });
