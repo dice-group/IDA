2faf43a3b6116bdd62831bcd101a6d59258803df
nandeesh <nandeesh@campus.uni-paderborn.de>
2021-01-06 18:13:01 +0100

    resolving codacy issues

--- backend-server/src/test/java/org/dice/ida/util/ValidatorUtilTest.java
+++ backend-server/src/test/java/org/dice/ida/util/ValidatorUtilTest.java
@@ -11,17 +11,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
 public class ValidatorUtilTest {
-	private ChatMessageResponse chatMessageResponse;
 
 	@Test
 	void testPreActionValidation() {
 		Map<String, Object> payLoad = new HashMap<>();
-		chatMessageResponse = new ChatMessageResponse();
+		ChatMessageResponse chatMessageResponse = new ChatMessageResponse();
 		chatMessageResponse.setPayload(payLoad);
 		ValidatorUtil.preActionValidation(chatMessageResponse);
 		assertEquals(IDAConst.BOT_SOMETHING_WRONG, chatMessageResponse.getMessage());
@@ -43,27 +46,17 @@ public class ValidatorUtilTest {
 	}
 
 	@Test
-	void testAreParamsValid() throws IDAException{
+	void testAreParamsValid() throws IDAException {
 		List<String> columnList = new ArrayList<>();
-		IDAException exception = assertThrows(IDAException.class, () -> {
-			ValidatorUtil.areParametersValid("", "", null, false);
-		});
+		IDAException exception = assertThrows(IDAException.class, () -> ValidatorUtil.areParametersValid("", "", null, false));
 		assertEquals(IDAConst.BOT_LOAD_DS_BEFORE, exception.getMessage());
-		exception = assertThrows(IDAException.class, () -> {
-			ValidatorUtil.areParametersValid("testDS", "", columnList, false);
-		});
+		exception = assertThrows(IDAException.class, () -> ValidatorUtil.areParametersValid("testDS", "", columnList, false));
 		assertEquals(IDAConst.DS_DOES_NOT_EXIST_MSG, exception.getMessage());
-		exception = assertThrows(IDAException.class, () -> {
-			ValidatorUtil.areParametersValid("covid19", "", columnList, false);
-		});
+		exception = assertThrows(IDAException.class, () -> ValidatorUtil.areParametersValid("covid19", "", columnList, false));
 		assertEquals(IDAConst.BOT_SELECT_TABLE, exception.getMessage());
-		exception = assertThrows(IDAException.class, () -> {
-			ValidatorUtil.areParametersValid("covid19", "testTable", columnList, false);
-		});
+		exception = assertThrows(IDAException.class, () -> ValidatorUtil.areParametersValid("covid19", "testTable", columnList, false));
 		assertEquals(IDAConst.TABLE_DOES_NOT_EXIST_MSG, exception.getMessage());
-		exception = assertThrows(IDAException.class, () -> {
-			ValidatorUtil.areParametersValid("covid19", "testTable", columnList, false);
-		});
+		exception = assertThrows(IDAException.class, () -> ValidatorUtil.areParametersValid("covid19", "testTable", columnList, false));
 		assertEquals(IDAConst.TABLE_DOES_NOT_EXIST_MSG, exception.getMessage());
 		columnList.add("cluster");
 		List<Map<String, String>> response = ValidatorUtil.areParametersValid("covid19", "Case_Time_Series.csv", columnList, true);
