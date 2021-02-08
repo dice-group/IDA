3cd831c3938f0bf0d52f37b19a85c9af171c02bf
Maqbool ur Rahim Khan <maqboolurrahim@gmail.com>
2021-02-08 15:45:52 +0100

    feat(ida-149): partial codacy compliances

--- backend-server/src/test/java/org/dice/ida/action/ScatterPlotActionTest.java
+++ backend-server/src/test/java/org/dice/ida/action/ScatterPlotActionTest.java
@@ -3,8 +3,6 @@ package org.dice.ida.action;
 import org.dice.ida.controller.MessageController;
 import org.dice.ida.model.ChatMessageResponse;
 import org.dice.ida.model.ChatUserMessage;
-import org.dice.ida.model.bubblechart.BubbleChartData;
-import org.dice.ida.model.bubblechart.BubbleChartItem;
 import org.dice.ida.model.scatterplot.ScatterPlotData;
 import org.dice.ida.model.scatterplot.ScatterPlotItem;
 import org.dice.ida.util.SessionUtil;
@@ -13,18 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class ScatterPlotActionTest {
-	private ChatUserMessage chatUserMessage;
-	private ChatMessageResponse chatMessageResponse;
 	@Autowired
 	private MessageController messageController;
 	@Autowired
@@ -32,7 +25,7 @@ public class ScatterPlotActionTest {
 
 	@Test
 	void testScatterPlotFlow() throws Exception {
-		chatUserMessage = new ChatUserMessage();
+		ChatUserMessage chatUserMessage = new ChatUserMessage();
 		chatUserMessage.setMessage("draw scatter plot");
 		chatUserMessage.setActiveDS("covid19");
 		chatUserMessage.setActiveTable("Case_Time_Series.csv");
@@ -48,7 +41,8 @@ public class ScatterPlotActionTest {
 		chatUserMessage.setMessage("total confirm cases");
 		messageController.handleMessage(chatUserMessage).call();
 		chatUserMessage.setMessage("average");
-		chatMessageResponse = messageController.handleMessage(chatUserMessage).call();
+
+		ChatMessageResponse chatMessageResponse = messageController.handleMessage(chatUserMessage).call();
 		ScatterPlotData scatterPlotData = (ScatterPlotData) chatMessageResponse.getPayload().get("scatterPlotData");
 		List<ScatterPlotItem> scatterPlotItemList = new ArrayList<>();
 		scatterPlotItemList.add(new ScatterPlotItem("0.0 - 9.0", 1.3333333333333333));
