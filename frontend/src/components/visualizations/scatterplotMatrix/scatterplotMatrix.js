19497660e9470b382cf7929597b982557112f7e7
nandeesh <nandeesh@campus.uni-paderborn.de>
2021-02-12 15:11:44 +0100

    Integrating scatterplot matrix UI with backend

--- frontend/src/components/visualizations/scatterplotMatrix/scatterplotMatrix.js
+++ frontend/src/components/visualizations/scatterplotMatrix/scatterplotMatrix.js
@@ -1,36 +1,73 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
-import "./scatterplot.css";
-import { Grid } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Chip from "@material-ui/core/Chip";
+import { Hidden } from "@material-ui/core";
+
+import "./scatterplotMatrix.css";
+import { IDA_CONSTANTS } from "./../../constants";
 
 export default class IDAScatterPlotMatrix extends Component {
-  margin = {
-    top: 20,
-    right: 0,
-    bottom: 100,
-    left: 100
-  };
-  height = 700;
-  graphData = {};
-  containerId = "";
+	margin = {
+		top: 20,
+		right: 0,
+		bottom: 100,
+		left: 100
+	};
+	height = 700;
+	width = 1000;
+	graphData = {};
+	containerId = "";
+	colorFunction = (label) => null;
+	tooltip = null;
 
-  constructor(props) {
-    super(props);
-    this.containerId = props.nodeId;
-    this.graphData = props.data;
-  }
+	constructor(props) {
+		super(props);
+		this.containerId = props.nodeId;
+		this.graphData = props.data;
+		this.state = {
+			referenceValues: []
+		};
+		this.tooltip = document.createElement('div');
+		this.tooltip.setAttribute('class', 'tooltip');
+		document.body.appendChild(this.tooltip);
+	}
 
-  componentDidMount() {
-    this.graphData && this.graphData.items && this.drawScatterPlot();
-  }
+	componentDidMount() {
+		if (this.graphData && this.graphData.items) {
+			this.graphData.columns.forEach(col => {
+				this.graphData.items = this.graphData.items.filter(entry => entry[`${col}`] !== IDA_CONSTANTS.UNKNOWN_LABEL);
+			});
+			const ref_column = this.graphData.referenceColumn;
+			this.colorFunction = d3.scaleOrdinal()
+				.domain(this.graphData.items.map(d => d[`${ref_column}`]))
+				.range(d3.schemeCategory10);
+			this.setState({
+				referenceValues: this.colorFunction.domain()
+			})
+			this.drawScatterPlot();
+		}
+	}
 
 	drawScatterPlot() {
-		const width = 954;
-		const columns = this.graphData.columns;
+		const self = this;
 		const ref_column = this.graphData.referenceColumn;
-		const padding = 20;
+		const columns = this.graphData.columns;
+		const padding = 50;
 		const size =
-			(width - (columns.length + 1) * padding) / columns.length + padding;
+			(this.width - (columns.length + 1) * padding) / columns.length + padding;
+		this.graphData.items = this.graphData.items.map(entry => {
+			let obj = {};
+			columns.forEach(c => {
+				obj[`${c}`] = parseFloat(entry[`${c}`]);
+			});
+			obj[`${ref_column}`] = entry[`${ref_column}`];
+			return obj;
+		});
 		const data = this.graphData.items;
 
 		const x = columns.map(c =>
@@ -41,19 +78,13 @@ export default class IDAScatterPlotMatrix extends Component {
 		);
 
 		const y = x.map(x => x.copy().range([size - padding / 2, padding / 2]));
-		const z = d3
-			.scaleOrdinal()
-			.domain(data.map(d => d[ref_column]))
-			.range(d3.schemeCategory10);
+		const z = this.colorFunction;
 
-		const svg = d3
-			.select("#sdsd")
-			.attr("viewBox", `${-padding} 0 ${width} ${width}`)
+		const svg = d3.select("#" + this.containerId)
+			.append("svg")
+			.attr("viewBox", `${-padding} 0 ${this.width} ${this.width}`)
 			.style("max-width", "100%")
 			.style("height", "auto");
-
-
-		// Labels for x axis
 
 		const xAxis = d3
 			.axisBottom()
@@ -65,14 +96,12 @@ export default class IDAScatterPlotMatrix extends Component {
 				.data(x)
 				.join("g")
 				.attr("transform", (d, i) => `translate(${i * size},0)`)
-				.each(function(d) {
+				.each(function (d) {
 					return d3.select(this).call(xAxis.scale(d));
 				})
 				.call(g => g.select(".domain").remove())
-				.call(g => g.selectAll(".tick line").attr("stroke", "#fff"));
+				.call(g => g.selectAll(".tick line").attr("stroke", "#efefef"));
 		});
-
-		// lables foe y-axis
 
 		const yAxis = d3
 			.axisLeft()
@@ -83,12 +112,12 @@ export default class IDAScatterPlotMatrix extends Component {
 			g.selectAll("g")
 				.data(y)
 				.join("g")
-				.attr("transform", (d, i) => `translate(0,${i * size})`)
-				.each(function(d) {
+				.attr("transform", (d, i) => `translate(0, ${i * size})`)
+				.each(function (d) {
 					return d3.select(this).call(yAxis.scale(d));
 				})
 				.call(g => g.select(".domain").remove())
-				.call(g => g.selectAll(".tick line").attr("stroke", "#fff"));
+				.call(g => g.selectAll(".tick line").attr("stroke", "#efefef"));
 		});
 
 		const cell = svg
@@ -107,20 +136,31 @@ export default class IDAScatterPlotMatrix extends Component {
 			.attr("width", size - padding)
 			.attr("height", size - padding);
 
-		cell.each(function([i, j]) {
+		cell.each(function ([i, j]) {
 			d3.select(this)
 				.selectAll("circle")
 				.data(data.filter(d => !isNaN(d[columns[i]]) && !isNaN(d[columns[j]])))
 				.join("circle")
 				.attr("cx", d => x[i](d[columns[i]]))
-				.attr("cy", d => y[j](d[columns[j]]));
+				.attr("cy", d => y[j](d[columns[j]]))
+				.attr("data-tooltip", d => columns[i] + ": " + d[columns[i]] + "\n" + columns[j] + ": " + d[columns[j]])
+				.on("mouseover", (event) => {
+					self.tooltip.style.display = "block";
+					self.tooltip.style.position = "absolute";
+					self.tooltip.style.top = event.clientY + "px";
+					self.tooltip.style.left = event.clientX + "px";
+					self.tooltip.innerText = event.srcElement.getAttribute("data-tooltip");
+				})
+				.on("mouseout", () => {
+					self.tooltip.style.display = "none";
+				});
 		});
 
 		const circle = cell
 			.selectAll("circle")
 			.attr("r", 3.5)
 			.attr("fill-opacity", 0.7)
-			.attr("fill", d => z(d.species));
+			.attr("fill", d => z(d[`${ref_column}`]));
 
 		svg
 			.append("g")
@@ -135,12 +175,48 @@ export default class IDAScatterPlotMatrix extends Component {
 			.text(d => d);
 	}
 
-  render() {
-    return <Grid>
-      <Grid item xs={12}>
-        <svg className="scatterplot-container" id={this.containerId}>
-        </svg>
-      </Grid>
-    </Grid>;
-  }
+	render() {
+		return <>
+			<Grid container>
+				<Hidden mdUp>
+					<Grid item xs={12}>
+						<div className="m-2">
+							{
+								this.state.referenceValues.map((label) => (
+									<Chip
+										key={label}
+										size="small"
+										avatar={<span className="legend-item-sm-icon mr-1" style={{ backgroundColor: this.colorFunction(label) }} />}
+										label={label}
+										className="mr-2 mt-2"
+									/>
+								))
+							}
+						</div>
+					</Grid>
+				</Hidden>
+				<Grid item md={9} className="tab-container">
+					<div className="scatterplot-matrix-container" id={this.containerId}></div>
+				</Grid>
+				<Hidden mdDown>
+					<Grid item md={3}>
+						<div>
+							<List component="nav" dense={true} aria-label="graph legend" className="grouped-bar-chart-legend">
+								{
+									this.state.referenceValues.map((label) => (
+										<ListItem key={label}>
+											<ListItemAvatar>
+												<div className="legend-item-icon" style={{ backgroundColor: this.colorFunction(label) }}></div>
+											</ListItemAvatar>
+											<ListItemText primary={label} />
+										</ListItem>
+									))
+								}
+							</List>
+						</div>
+					</Grid>
+				</Hidden>
+			</Grid>
+		</>;;
+	}
 }
