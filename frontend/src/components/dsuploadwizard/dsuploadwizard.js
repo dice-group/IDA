1b8d5f1a0b89624e25b831df2a9429cdae92e268
Maqbool ur Rahim Khan <maqboolurrahim@gmail.com>
2021-03-11 18:12:13 +0100

    feat(ida-171): multiple file upload functionality added

--- frontend/src/components/dsuploadwizard/dsuploadwizard.js
+++ frontend/src/components/dsuploadwizard/dsuploadwizard.js
@@ -15,6 +15,11 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import axios from "axios";
 
@@ -26,21 +31,22 @@ export default class DSUploadWizard extends React.Component {
 			activeStep: 0,
 			steps: ["Upload dataset", "confirm meta data", "finalize"],
 			isFileSelected: false,
-			fileName: '',
-			file: null,
+			files: [],
 			nextButtonText: 'Upload',
 			enableNextButton: false,
 			showError: false,
-			showFileUploadLoading: false
+			showFileUploadLoading: false,
+			metaData: null
 		};
 	}
 
 	onFileChange = (ev) => {
-		const files = ev.target.files
+		const files = [];
+		Array.from(ev.target.files).forEach(f => files.push(f));
 		if (files.length > 0) {
-			this.setState({isFileSelected: true, file: files[0], fileName: files[0].name, enableNextButton: true});
+			this.setState({isFileSelected: true, files: files, enableNextButton: true});
 		} else {
-			this.setState({isFileSelected: false, enableNextButton: false, fileName: '', file: null});
+			this.setState({isFileSelected: false, enableNextButton: false, files: []});
 		}
 	}
 
@@ -48,7 +54,10 @@ export default class DSUploadWizard extends React.Component {
 		if (this.state.activeStep === 0) {
 			this.setState({showFileUploadLoading: true, enableNextButton: false})
 			let formData = new FormData();
-			formData.append("file", this.state.file);
+			const files = this.state.files;
+			for (let i = 0; i < files.length; i++) {
+				formData.append(`files[${i}]`, files[i])
+			}
 			axios.post("http://127.0.0.1:5000/", formData, {
 				headers: {
 					"Content-Type": "multipart/form-data",
@@ -65,22 +74,124 @@ export default class DSUploadWizard extends React.Component {
 		this.setState({showError: false})
 	}
 
+	handleChange = (ev) => {
+		console.log(ev)
+	}
+
+	removefile = (index) => {
+		const files = this.state.files;
+		files.splice(index, 1);
+		this.setState({files: files});
+		if (! files.length) {
+			this.setState({isFileSelected: false})
+		}
+	}
+
 	render() {
-		const renderFileUploadText = () => {
+		const renderFileUpload = () => {
 			if (!this.state.showFileUploadLoading) {
 				if (!this.state.isFileSelected) {
 					return (
-						<div>
-							<div style={{textAlign: 'center'}}>Select dataset (single file or zip).. <br/>You can select
-								.csv, .json, .xlsx or compressed zip file
+						<div className="upload-dataset-box">
+							<input
+								type="file"
+								accept="text/csv"
+								id="icon-button-file"
+								onChange={this.onFileChange}
+								multiple
+								hidden
+							/>
+							<label htmlFor="icon-button-file" style={{ marginBottom: '-15px'}} style={{display: !this.state.showFileUploadLoading ? "block" : "none"}}>
+								<IconButton color="primary" aria-label="upload picture" component="span">
+									<BackupIcon style={{ fontSize: 80 }}/>
+								</IconButton>
+							</label>
+							<CircularProgress style={{display: this.state.showFileUploadLoading ? "block" : "none"}} />
+							<div style={{textAlign: 'center'}}>Select dataset (single file or multiple).. <br/>You can select
+								.csv files
 							</div>
 						</div>
 					)
 				} else {
-					return <div>File selected: {this.state.fileName}</div>
+					let files_row = []
+					for (var i = 0; i < this.state.files.length; i++) {
+						let a = i;
+						files_row.push(<ListItem><ListItemText primary={this.state.files[i].name} /><ListItemSecondaryAction><IconButton edge="end" aria-label="delete"><DeleteIcon onClick={() => { this.removefile(a) } } /></IconButton></ListItemSecondaryAction></ListItem>)
+					}
+					return <div><h5 style={{padding: '10px 0', textAlign: 'center', color: '#444'}}>Selected file(s)</h5><List>{files_row}</List></div>
 				}
 			} else {
-				return <div>Uploading {this.state.fileName} ... </div>
+				return <div className="upload-dataset-box"><CircularProgress /></div>
+			}
+		}
+
+		const renderMetaDataForm = () => {
+			if (this.state.metaData) {
+				return (<div className="meta-data-box">
+					<form onChange={this.handleChange}>
+					<table>
+						<tr>
+							<td width="15%" className="heading">Dataset name</td>
+							<td>{this.state.metaData.dsName}</td>
+						</tr>
+						<tr>
+							<td  width="15%" className="heading">Dataset description</td>
+							<td><input type="text" value={this.state.metaData.dsDesc}/></td>
+						</tr>
+					</table>
+					<br/>
+					This dataset contains {this.state.metaData.filesMd.length} files.
+					<br/>
+					<hr/>
+					<table>
+						<tr>
+							<td className="heading">File name</td>
+							<td>{this.state.metaData.filesMd[0].fileName}</td>
+						</tr>
+						<tr>
+							<td className="heading">Display name</td>
+							<td><input type="text" value={this.state.metaData.filesMd[0].displayName} /></td>
+						</tr>
+						<tr>
+							<td className="heading">File description</td>
+							<td><input type="text" value={this.state.metaData.filesMd[0].fileDesc} /></td>
+						</tr>
+						<tr>
+							<td className="heading">Columns count</td>
+							<td>{this.state.metaData.filesMd[0].colCount}</td>
+						</tr>
+						<tr>
+							<td className="heading">Row count</td>
+							<td>{this.state.metaData.filesMd[0].rowCount}</td>
+						</tr>
+					</table>
+					<table>
+						<thead>
+						<td className="heading">Column index</td>
+						<td className="heading">Column name</td>
+						<td className="heading">Column description</td>
+						<td className="heading">Column attribute</td>
+						<td className="heading">Column type</td>
+						<td className="heading">Contains unique values</td>
+						</thead>
+						<tbody>
+						{ this.state.metaData.filesMd[0].fileColMd.map((e, i) => {
+							return (
+								<tr key={i}>
+									<td>{e.colIndex}</td>
+									<td>{e.colName}</td>
+									<td><input value={e.colDesc} /></td>
+									<td><input value={e.colAttr} /></td>
+									<td>{e.colType}</td>
+									<td>{e.isUnique? 'Yes' : 'No'}</td>
+								</tr>
+							)
+						})}
+						</tbody>
+
+					</table>
+					</form>
+				</div>)
 			}
 		}
 
@@ -114,34 +225,17 @@ export default class DSUploadWizard extends React.Component {
 					<DialogContent>
 						<DialogContentText id="alert-dialog-description">
 							<div style={{display: this.state.activeStep === 0 ? "block" : "none"}}>
-								<div className="upload-dataset-box">
-									<input
-										type="file"
-										accept="text/csv, .json, .xlsx, application/zip"
-										id="icon-button-file"
-										onChange={this.onFileChange}
-										hidden
-									/>
-									<label htmlFor="icon-button-file" style={{ marginBottom: '-15px'}} style={{display: !this.state.showFileUploadLoading ? "block" : "none"}}>
-										<IconButton color="primary" aria-label="upload picture" component="span">
-											<BackupIcon style={{ fontSize: 80 }}/>
-										</IconButton>
-									</label>
-									<CircularProgress style={{display: this.state.showFileUploadLoading ? "block" : "none"}} />
-									{renderFileUploadText()}
-									<Snackbar
-										open={this.state.showError}
-										onClose={this.hideError}
-										autoHideDuration="4000"
-									>
-										<Alert severity="error">Unable to upload dataset. Please try again..</Alert>
-									</Snackbar>
-								</div>
+								{renderFileUpload()}
+								<Snackbar
+									open={this.state.showError}
+									onClose={this.hideError}
+									autoHideDuration="6000"
+								>
+									<Alert severity="error">Unable to upload dataset. Please try again..</Alert>
+								</Snackbar>
 							</div>
 							<div style={{display: this.state.activeStep === 1 ? "block" : "none"}}>
-								<div className="meta-data-box">
-									here is meta data
-								</div>
+								{renderMetaDataForm()}
 							</div>
 						</DialogContentText>
 					</DialogContent>
--- pydsmx/app.py
+++ pydsmx/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 cors = CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['UPLOAD_FOLDER'] = './uploads'
+app.config['TEMP_FOLDER'] = './temp-uploads'
 app.config['MAX_CONTENT_LENGTH'] = 5000000 # Maximum fi
 
 
@@ -16,33 +16,36 @@ app.config['MAX_CONTENT_LENGTH'] = 5000000 # Maximum fi
 @cross_origin()
 def upload_file():
 	if request.method == 'POST':
-		file = request.files['file']
-		if file:
-			file_name = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+		files = request.files.to_dict() # convert multidict to dict
+		for file in files:
+			file_name = secure_filename(files[file].filename)
+			files[file].save(os.path.join(app.config['TEMP_FOLDER'], file_name))
+		return 'Done', 200
+		# if file:
 
-			ds = pd.read_csv('./uploads/' + file_name)
-			cols = [col for col in ds.columns]
-			no_rows, no_cols = ds.shape
-
-			file_cols_md = []
-			for index, col_name in enumerate(cols):
-				file_cols_md.append({"colIndex": index+1, "colName": col_name, "colDesc":  col_name, "colType": str(ds[col_name].dtype), "colAttr": col_name, "isUnique": pd.Series(ds[col_name]).is_unique})
-
-			files_meta_data = [
-				{
-					"fileName": file_name,
-					"displayName": file_name,
-					"fileDesc": "",
-					"rowCount": no_rows,
-					"colCount": no_cols,
-					"fileColMd": file_cols_md
-				}
-			]
-			meta_data = {
-				"dsName": file_name,
-				"dsDesc": "",
-				"filesMd": files_meta_data
-			}
-			dict = {"metadata": meta_data, "message": "dataset to uploaded successfully"}
-			return dict, 200
+		#
+		# 	ds = pd.read_csv('./temp-uploads/' + file_name)
+		# 	cols = [col for col in ds.columns]
+		# 	no_rows, no_cols = ds.shape
+		#
+		# 	file_cols_md = []
+		# 	for index, col_name in enumerate(cols):
+		# 		file_cols_md.append({"colIndex": index+1, "colName": col_name, "colDesc":  col_name, "colType": str(ds[col_name].dtype), "colAttr": col_name, "isUnique": pd.Series(ds[col_name]).is_unique})
+		#
+		# 	files_meta_data = [
+		# 		{
+		# 			"fileName": file_name,
+		# 			"displayName": file_name,
+		# 			"fileDesc": "",
+		# 			"rowCount": no_rows,
+		# 			"colCount": no_cols,
+		# 			"fileColMd": file_cols_md
+		# 		}
+		# 	]
+		# 	meta_data = {
+		# 		"dsName": file_name,
+		# 		"dsDesc": "",
+		# 		"filesMd": files_meta_data
+		# 	}
+		# 	dict = {"metadata": meta_data, "message": "dataset to uploaded successfully"}
+		# 	return dict, 200
