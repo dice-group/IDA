ec1bd8e05b360ff8394d96fd3be9f10f612ad61f
nandeesh <nandeesh@campus.uni-paderborn.de>
2021-06-04 16:46:23 +0200

    Adding more testcases

--- backend-server/src/test/java/org/dice/ida/util/DialogflowAdminUtilTest.java
+++ backend-server/src/test/java/org/dice/ida/util/DialogflowAdminUtilTest.java
@@ -1,5 +1,7 @@
 package org.dice.ida.util;
 
+import org.dice.ida.controller.MessageController;
+import org.dice.ida.model.EntityUpdateRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,15 +12,18 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 public class DialogflowAdminUtilTest {
 
 	@Autowired
-	private DialogFlowAdminUtil dialogFlowAdminUtil;
+	private MessageController messageController;
 
 	@Test
 	void addValuesToEntityTest() throws Exception {
+		EntityUpdateRequest entityUpdateRequest = new EntityUpdateRequest();
 		Map<String, List<String>> entities = new HashMap<>(){{
 			put("Detected State", new ArrayList<>(){{
 				add("Detected State");
@@ -29,21 +34,34 @@ public class DialogflowAdminUtilTest {
 				add("date");
 			}});
 		}};
-		String actualResponse = dialogFlowAdminUtil.addValuesToEntity("column_name", entities);
+		entityUpdateRequest.setEntityId("column_name");
+		entityUpdateRequest.setEntityList(entities);
+		String actualResponse = messageController.addColumnEntities(entityUpdateRequest);
 		String expectedResponse = "Following columns already exist: [Detected State, Date]\n All other columns were added Successfully";
 		assertEquals(expectedResponse, actualResponse);
 	}
 
 	@Test
 	void invalidEntityIdTest() throws Exception {
+		EntityUpdateRequest entityUpdateRequest = new EntityUpdateRequest();
 		Map<String, List<String>> entities = new HashMap<>(){{
 			put("Test Column", new ArrayList<>(){{
 				add("Test Column");
 				add("testcolumn");
 			}});
 		}};
-		String actualResponse = dialogFlowAdminUtil.addValuesToEntity("test_entity", entities);
+		entityUpdateRequest.setEntityId("test_entity");
+		entityUpdateRequest.setEntityList(entities);
+		String actualResponse = messageController.addColumnEntities(entityUpdateRequest);
 		String expectedResponse = "Given entity id does not exist";
 		assertEquals(expectedResponse, actualResponse);
+	}
+
+	@Test
+	void doesDatasetExistTest() throws Exception {
+		boolean doesDatasetExist = messageController.checkDatasetExist("covid19");
+		assertTrue(doesDatasetExist);
+		doesDatasetExist = messageController.checkDatasetExist("election");
+		assertFalse(doesDatasetExist);
 	}
 }
